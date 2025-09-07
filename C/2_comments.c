d7b4b929567c47fa474790bae0bdb3491735ea80
Aditya Kumar Singh <adityasinghffjuvi@gmail.com>
2025-09-07 23:28:20 +0530

    2_comments

---
+++ C/2_comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // This is a single-line comment
+
+    printf("Hello World!\n");
+    return 0;
+
+    /* this is a multi-line comment
+    
+    above command prints Hello World to the console.
+    */
+}
