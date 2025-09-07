18682fe7c7b84afdfdb2398a81b8465e40712b1b
Aditya Kumar Singh <adityasinghffjuvi@gmail.com>
2025-09-07 23:50:01 +0530

    variables1

---
+++ C/3_variables1.c
@@ -0,0 +1,20 @@
+/* 
+Variables:- 
+
+Variables are used to store data in a program. In C, you must declare a variable before using it,
+specifying its type and name. Common data types include int (integer), float (floating-point number),
+char (character), and more. 
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Variables in C\n");
+    int myNum = 5;
+    printf("int variable: %d\n", myNum);
+    float myFloat = 3.14;
+    printf("float variable: %.2f\n", myFloat);
+    char myChar = 'A';
+    printf("char variable: %c\n", myChar);
+    return 0;
+}
