c19de36bb249bd2fd27069469c2d4effe5a73265
Aditya Kumar Singh <adityasinghffjuvi@gmail.com>
2025-09-07 23:54:08 +0530

    variables edited

--- C/3_variables1.c
+++ C/3_variables1.c
@@ -4,6 +4,10 @@ Variables:-
 Variables are used to store data in a program. In C, you must declare a variable before using it,
 specifying its type and name. Common data types include int (integer), float (floating-point number),
 char (character), and more. 
+for example: 
+to print an integer variable, you can use the %d format specifier in printf.
+to print a float variable, you can use the %f format specifier in printf.
+to print a char variable, you can use the %c format specifier in printf.
 */
 
 #include <stdio.h>
